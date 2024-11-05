990f608001796941a73ca29ac858bfe0261b7a05
xQrewo <78736102+urewo396@users.noreply.github.com>
2024-11-05 15:08:57 +0000

    .

--- Application1/main.cpp
+++ Application1/main.cpp
@@ -18,7 +18,7 @@ public:
 
 private:
     void start_accept() {
-        // Create a new socket using the executor's context
+        // Create a new socket using the io_context directly
         auto new_connection = make_shared<tcp::socket>(acceptor_.get_executor().context());
         acceptor_.async_accept(*new_connection,
             [this, new_connection](const boost::system::error_code& error) {
